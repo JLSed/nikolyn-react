b5001d4c95641c96ba49d7a85e75d547f9ff410a
JLSed <seddd001@gmail.com>
2025-04-25 22:50:30 +0800

    fixed issue with router not working properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import CashierPage from "./pages/CashierPage";
 
